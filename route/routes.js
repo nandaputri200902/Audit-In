7c9c95255a3169a902216a2757310069bc7b0c16
Hasimy <ahmadhasyimi674@gmail.com>
2019-10-22 21:28:10 +0700

    tambah kirim kontak imeil gann

--- route/routes.js
+++ route/routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mailer from 'nodemailer';
 import db from '../database/mysql';
 
 const Route = express.Router();
@@ -8,5 +9,28 @@ Route.get('/aula', (req, res) => res.render('aula'));
 Route.get('/login', (req, res) => res.render('login'));
 Route.get('/bantuan', (req, res) => res.render('bantuan'));
 Route.get('/tim', (req, res) => res.render('tim'));
+
+Route.post('/kirimEmail', (req, res) => {
+  let transporter = mailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    auth: {
+      user: 'emailnya@email.com', //email pengirim
+      pass: 'password.email' //pass pengirim
+    }
+  });
+  let pengaturanEmail = {
+    from: req.body.from,
+    to: 'tujuan@email.com', //tujuan kirim email
+    subject: req.body.subject,
+    text: req.body.body
+  };
+
+  transporter.sendMail(pengaturanEmail, (err, info) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
 
 export default Route;
